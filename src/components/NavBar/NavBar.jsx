22f4b7539440f8915c328dd8000f541f1e848ada
Coleman Grustas <grustas.c@northeastern.edu>
2025-12-06 11:54:18 -0500

    feat: style navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar({ numCartItems }) {
   return (
-    <nav className="navbar">
-      <ul>
+    <nav className={styles.navBar}>
+      <h1 className={styles.title}>Fake Business</h1>
+      <ul className={styles.linksList}>
         <li>
-          <Link to="/">Home</Link>
+          <Link className={styles.link} to="/">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/shop">Shop</Link>
+          <Link className={styles.link} to="/shop">
+            Shop
+          </Link>
         </li>
         <li>
-          <Link to="/cart">Cart ({numCartItems})</Link>
+          <Link className={styles.link} to="/cart">
+            Cart ({numCartItems})
+          </Link>
         </li>
       </ul>
     </nav>
