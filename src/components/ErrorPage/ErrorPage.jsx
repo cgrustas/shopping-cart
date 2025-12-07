098ca24d393827e9888c275c5f0a4d7b4e1171df
Coleman Grustas <grustas.c@northeastern.edu>
2025-12-07 07:59:11 -0500

    feat: style error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,13 @@
+import styles from "./ErrorPage.module.css";
+import { Link } from "react-router";
+
 export default function ErrorPage() {
   return (
-    <div>
-      <h2>Error: this route doesn't exist!</h2>
+    <div className={styles.errorPage}>
+      <h2 className={styles.errorMessage}>Error: this route doesn't exist!</h2>
+      <Link className={styles.link} to="/">
+        You can go back to the home page by clicking here, though!
+      </Link>
     </div>
   );
 }
